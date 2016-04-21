0819a69bba60185c7d32eec12a1619f37f8293b6
Shubh303 <10023271@sbstudents.org>
2016-04-21 18:41:25 -0400

    Added files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,26 @@ $(document).ready(function() {
 
   var playerName = '';
   var playerAnswer1 = '';
+  var playerAnswer2 = '';
   var answer1 = 'number four, privet drive'
+  var answer2 = 'hagrid';
   var correctCounter = 0;
+  var triesLeft = 3
 
   // Hide Player Name
   $('#dashboard').hide();
   $('.question').hide();
   $('.correct-msg').hide();
   $('.incorrect-msg').hide();
+  $('#lose-game-msg').hide();
+
+
+
+  /****************************************************************************
+  Dashboard/ Welcome Screen
+  ****************************************************************************/
+
+
 
   // Allow user to type name immediately on page load
   $('#player-name').focus();
@@ -38,10 +50,19 @@ $(document).ready(function() {
     playerName = $('#player-name').val();
     console.log(playerName);
     $('#show-player-name').text(playerName);
-    $('#welcome-screen').addClass('animated zoomOut');
+    $('#welcome-screen').addClass('animated fadeOut');
+    $('#tries-left').text(triesLeft);
     setTimeout(removeWelcomeScreen, 1000);
     setTimeout(removedquestion, 2000);
   });
+
+
+
+  /****************************************************************************
+  QUESTION 1
+  ****************************************************************************/
+
+
 
   // Store answer 1
   $('#question1-btn').on('click', function(e) {
@@ -58,17 +79,104 @@ $(document).ready(function() {
     $('#correct-answer1').text(answer1);
   };
 
+  // Fade out answer check message
+  function fadeOutAnswerCheckMsg() {
+  	$('#incorrect-msg1').removeClass('fadeIn').addClass('fadeOut');
+  };
+
+  function fadeOutQuestion1() {
+  	$('#question1').addClass('animated fadeOut');
+  	setTimeout($('#question1').detach(), 2500);
+  	$('#question2').show().addClass('animated fadeIn');
+  };
+
+  function triesLeftCountdown() {
+  	triesLeft--;
+  	$('#tries-left').text(triesLeft);
+  	if(triesLeft === 0) {
+  		$('.question').addClass('animated fadeOut');
+  		$('.question').detach();
+  		$('#lose-game-msg').show().addClass('animated fadeIn');
+  	};
+  };
+
   function answerCheck1() {
+  	// If player gets question right...
     if(playerAnswer1 === answer1) {
       console.log("Player's answer is correct!");
       $('#correct-msg1').show().addClass('animated pulse');
       correctCounter++;
       $('#score').text(correctCounter);
+      setTimeout(fadeOutQuestion1, 2500);
     }
     else {
+    	// If player gets question wrong...
       console.log("Player's answer is incorrect!");
       $('#incorrect-msg1').show().addClass('animated tada');
+      triesLeftCountdown();
+      setTimeout(fadeOutAnswerCheckMsg, 2500);
     }
   };
 
+
+
+  /****************************************************************************
+  QUESTION 2
+  ****************************************************************************/
+
+
+
+  // Store answer 2
+  $('#question2-btn').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerAnswer2 = $('#input-question2').val().trim().toLowerCase(); // This assigns the user's input to the variable playerAnswer2
+    console.log("The Player's answer for question 2 is " + playerAnswer2);
+    // showPlayerAnswer2(); // This first showPlayerAnswer2 function
+    answerCheck2();
+  });
+
+  // Show answers
+  function showPlayerAnswer2() {
+    $('#show-player-answer2').text(playerAnswer2);
+    $('#correct-answer2').text(answer2);
+  };
+
+  // Fade out answer check message
+  function fadeoutAnswerCheckMsg() {
+  	$('#incorrect-msg2').removeClass('fadeIn').addClass('fadeOut');
+  };
+
+  function fadeOutQuestion2() {
+  	$('#question2').addClass('animated fadeOut');
+  	setTimeout($('#question2').detach(), 2500);
+  	$('#question3').show().addClass('animated fadeIn');
+  };
+
+  function triesLeftCountdown() {
+  	triesLeft--;
+  	$('#tries-left').text(triesLeft);
+  	if(triesLeft === 0) {
+  		$('.question').addClass('animated fadeOut');
+  		$('.question').detach();
+  		$('#lose-game-msg').show().addClass('animated fadeIn');
+  	};
+  };
+
+  function answerCheck2() {
+  	// If player gets question right...
+    if(playerAnswer2 === answer2) {
+      console.log("Player's answer is correct!");
+      $('#correct-msg2').show().addClass('animated pulse');
+      correctCounter++;
+      $('#score').text(correctCounter);
+      setTimeout(fadeOutQuestion2, 2500);
+    }
+    else {
+    	// If player gets question wrong...
+      console.log("Player's answer is incorrect!");
+      $('#incorrect-msg2').show().addClass('animated tada');
+      triesLeftCountdown();
+      setTimeout(fadeoutAnswerCheckMsg, 2500);
+    }
+  };
 }); // ready function ends
